41d0dec0d49feee91ed00fce81830d1f9774259b
Nicholas Long <nick@glmps.com>
2015-08-10 13:35:17 -0700

    Added sort assets

---
+++ TeamLister/SplitViewController.m
@@ -0,0 +1,27 @@
+#import "SplitViewController.h"
+
+@interface SplitViewController ()
+
+@end
+
+@implementation SplitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(BOOL)shouldAutorotate {
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+@end
